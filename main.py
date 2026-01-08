ba0985aabb8a203de2401045cbb4376ffdd46d6d
NKsken <nkskenk@gmail.com>
2026-01-08 12:04:20 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sympy as sp
+
+# 미지수 x 정의
+x = sp.Symbol('x')
+
+f = x ** 2 + 3 * x + 5
+
+# x에 대해 미분
+der = sp.diff(f, x)
+print(f"미분 결과: {der}")          # 1
+
+# x에 대해 부정적분
+
+result = sp.integrate(der, x)
+
+print(result)                       # 2
+
+# 1의 결과 : 2*x + 3
+# 2의 결과 : x**2 + 3*x
+
+
+
